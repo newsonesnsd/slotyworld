04597d6d3d006d6d6310b73abd90155e5a9eda2b
newsonesnsd <newsonesnsd@gmail.com>
2017-11-26 05:00:39 +0700

    Track prodiuct unsent

--- admin/pages/tracking.php
+++ admin/pages/tracking.php
@@ -169,13 +169,7 @@
                 <!-- /.col-lg-12 -->
             </div>
             <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
 
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
             <!-- Show All Users -->
             <div class="row">
                 <div class="col-lg-12">
@@ -222,6 +216,48 @@
                         <!-- /.panel-body -->
                     </div>
                     <!-- /.panel -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Tracking
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="table-responsive">
+
+                                <table class="table table-striped table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Tracking Number</th>
+                                            <th>Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                            $sql = "SELECT i.invoiceid id, t.trackingnumber tracknum, st.description des from Invoice i join Tracking t on t.invoiceid = i.invoiceid
+                                                    join StatusTracking st on st.statustrackingid = t.statustrackingid
+                                                    where st.description = 'Preparing'
+                                                    order by invoiceid asc";
+                                            $result = $conn->query($sql);
+                                            if ($result->num_rows > 0) {
+                                                // output data of each row
+                                                while($row = $result->fetch_assoc()) {
+                                                    echo "<tr>";
+                                                    echo "<td>" .$row["id"]."</td>";
+                                                    echo "<td>" .$row["tracknum"]."</td>";
+                                                    echo "<td>" .$row["des"]."</td>";
+                                                    echo "</tr>";
+
+                                                }
+                                            }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.table-responsive -->
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
                 </div>
                 <!-- /.col-lg-6 -->
                 <!-- /.col-lg-6 -->
