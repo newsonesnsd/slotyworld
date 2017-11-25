424738484fe3b7a83fb70a2206a1d02ebef29a57
newsonesnsd <newsonesnsd@gmail.com>
2017-11-25 16:44:11 +0700

    Test count

--- test.php
+++ test.php
@@ -10,17 +10,21 @@
           if(!$conn){
             echo "เชื่อมต่อไม่ได้";
           }
-          $sql = "select * from User U join Role R on U.roleid = R.roleid  where R.roleid = 'Customers'";
+          $sql = "select COUNT(*) AS countUser from User U join Role R on U.roleid = R.roleid WHERE R.roleDescription = 'Customer' group by R.roleid";
           $result = $conn->query($sql);
-          if ($result->num_rows > 0) {
-              // output data of each row
-              while($row = $result->fetch_assoc()) {
-                  echo "id" .$row["userid"]."</br>";
-
-              }
+          while ($item = $result -> fetch_assoc()) {
+              # code...
+              $item = $item['countUser'];
+              echo $item;
+              break;
           }
-
-
+          // if ($result->num_rows > 0) {
+          //     // output data of each row
+          //     while($row = $result->fetch_assoc()) {
+          //         echo "" .$row["count(*)"]."</br>";
+          //
+          //     }
+          // }
 ?>
 <?php
     //statement
